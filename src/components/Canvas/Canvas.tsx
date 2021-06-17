73dd083220197f89b0c2dc3e427811b7bbcd0058
APreclin <amaurypreclin@gmail.com>
2021-06-17 15:21:31 +0200

    add color to canvas

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -4,103 +4,108 @@ import "./Canvas.css";
 import { Location, Request } from "../../models/solution";
 
 export interface CanvasProps {
-    origin: Location;
-    requests: Request[];
+	classname?: string;
+	color?: string;
+	origin: Location;
+	requests: Request[];
 }
 
 const Canvas: React.FC<CanvasProps> = (props) => {
-    const { origin, requests } = props;
-    const { x: xOrigin, y: yOrigin } = origin;
+	const { classname, color, origin, requests } = props;
+	const { x: xOrigin, y: yOrigin } = origin;
 
-    const canvasRef = React.useRef<HTMLCanvasElement>(null);
+	const canvasRef = React.useRef<HTMLCanvasElement>(null);
 
-    const drawRoute = React.useCallback(
-        (context: CanvasRenderingContext2D | null) => {
-            if (!context) return;
+	const drawRoute = React.useCallback(
+		(context: CanvasRenderingContext2D | null) => {
+			if (!context) return;
 
-            context.fillStyle = "rgba(255, 0, 0, 1)";
-            context.arc(xOrigin, yOrigin, 5, 0, 2 * Math.PI);
-            context.fill();
+			context.fillStyle = "rgba(255, 0, 0, 1)";
+			context.arc(xOrigin, yOrigin, 5, 0, 2 * Math.PI);
+			context.fill();
 
-            requests.forEach((request, index) => {
-                const previousLocation =
-                    index > 0 ? requests[index - 1].location : origin;
+			requests.forEach((request, index) => {
+				const previousLocation =
+					index > 0 ? requests[index - 1].location : origin;
 
-                const { location: currentLocation } = request;
-                const { x: xPrevious, y: yPrevious } = previousLocation;
-                const { x: xCurrent, y: yCurrent } = currentLocation;
+				const { location: currentLocation } = request;
+				const { x: xPrevious, y: yPrevious } = previousLocation;
+				const { x: xCurrent, y: yCurrent } = currentLocation;
 
-                context.beginPath();
-                context.fillStyle = "rgba(0, 0, 0, 1)";
-                context.arc(xCurrent, yCurrent, 4, 0, 2 * Math.PI);
-                context.fill();
-                context.lineWidth = 2;
-                context.moveTo(xPrevious, yPrevious);
-                context.lineTo(xCurrent, yCurrent);
-                context.stroke();
-            });
+				context.beginPath();
+				context.fillStyle = "rgba(0, 0, 0, 1)";
+				context.arc(xCurrent, yCurrent, 4, 0, 2 * Math.PI);
+				context.fill();
+				context.strokeStyle = color ?? "black";
+				context.lineWidth = 2;
+				context.moveTo(xPrevious, yPrevious);
+				context.lineTo(xCurrent, yCurrent);
+				context.stroke();
+			});
 
-            const lastRequest = requests[requests.length - 1];
-            const { location } = lastRequest;
-            const { x, y } = location;
-            context.beginPath();
-            context.lineWidth = 2;
-            context.moveTo(xOrigin, yOrigin);
-            context.lineTo(x, y);
-            context.stroke();
-        },
-        [requests, origin, xOrigin, yOrigin]
-    );
+			const lastRequest = requests[requests.length - 1];
+			const { location } = lastRequest;
+			const { x, y } = location;
+			context.beginPath();
+			context.lineWidth = 2;
+			context.moveTo(xOrigin, yOrigin);
+			context.lineTo(x, y);
+			context.stroke();
+		},
+		[requests, origin, xOrigin, yOrigin, color]
+	);
 
-    const drawGraph = React.useCallback(
-        (context: CanvasRenderingContext2D | null) => {
-            if (!context) return;
+	const drawGraph = React.useCallback(
+		(context: CanvasRenderingContext2D | null) => {
+			if (!context) return;
 
-            context.strokeStyle = "rgba(0, 0, 0, 0.3)";
-            context.strokeRect(0, 0, 800, 800);
+			context.strokeStyle = "rgba(0, 0, 0, 0.3)";
+			context.strokeRect(0, 0, 800, 800);
 
-            for (let index = 1; index < 16; index++) {
-                const pixel = index * 50;
-                context.beginPath();
+			for (let index = 1; index < 16; index++) {
+				const pixel = index * 50;
+				context.beginPath();
 
-                const isModulo100 = pixel % 100 === 0;
-                const littleLine = "rgba(0, 0, 0, 0.1)";
-                const bigLine = "rgba(0, 0, 0, 0.2)";
-                context.strokeStyle = isModulo100 ? bigLine : littleLine;
+				const isModulo100 = pixel % 100 === 0;
+				const littleLine = "rgba(0, 0, 0, 0.1)";
+				const bigLine = "rgba(0, 0, 0, 0.2)";
+				context.strokeStyle = isModulo100 ? bigLine : littleLine;
 
-                context.moveTo(pixel, 0);
-                context.lineTo(pixel, 800);
-                context.stroke();
-            }
+				context.moveTo(pixel, 0);
+				context.lineTo(pixel, 800);
+				context.stroke();
+			}
 
-            for (let index = 1; index < 16; index++) {
-                const pixel = index * 50;
-                context.beginPath();
+			for (let index = 1; index < 16; index++) {
+				const pixel = index * 50;
+				context.beginPath();
 
-                const isModulo100 = pixel % 100 === 0;
-                const littleLine = "rgba(0, 0, 0, 0.1)";
-                const bigLine = "rgba(0, 0, 0, 0.2)";
-                context.strokeStyle = isModulo100 ? bigLine : littleLine;
+				const isModulo100 = pixel % 100 === 0;
+				const littleLine = "rgba(0, 0, 0, 0.1)";
+				const bigLine = "rgba(0, 0, 0, 0.2)";
+				context.strokeStyle = isModulo100 ? bigLine : littleLine;
 
-                context.moveTo(0, pixel);
-                context.lineTo(800, pixel);
-                context.stroke();
-            }
-        },
-        []
-    );
+				context.moveTo(0, pixel);
+				context.lineTo(800, pixel);
+				context.stroke();
+			}
+		},
+		[]
+	);
 
-    React.useEffect(() => {
-        const canvas = canvasRef.current;
+	React.useEffect(() => {
+		const canvas = canvasRef.current;
 
-        if (!canvas) return;
+		if (!canvas) return;
 
-        const context = canvas.getContext("2d");
-        drawRoute(context);
-        drawGraph(context);
-    }, [drawRoute, drawGraph]);
+		const context = canvas.getContext("2d");
+		drawRoute(context);
+		drawGraph(context);
+	}, [drawRoute, drawGraph]);
 
-    return <canvas height="800" ref={canvasRef} width="800" />;
+	return (
+		<canvas className={classname} height="800" ref={canvasRef} width="800" />
+	);
 };
 
 export { Canvas };
--- src/components/Day/Day.tsx
+++ src/components/Day/Day.tsx
@@ -2,110 +2,163 @@ import React from "react";
 import { Collapse } from "react-collapse";
 
 import "./Day.css";
+import { Canvas } from "../Canvas";
+import { colors } from "./constants";
 import { DayModel } from "../../models/solution";
 import { DeliveryRound } from "../DeliveryRound";
 import { InstallationRound } from "../InstallationRound";
-import { GlobalCanvas } from "../GlobalCanvas/GlobalCanvas";
 
 export interface DayProps {
-    day: DayModel;
-    index: number;
+	day: DayModel;
+	index: number;
 }
 
 const Day: React.FC<DayProps> = (props) => {
-    const { day, index } = props;
-    const {
-        deliveryRounds,
-        installationRounds,
-        numberOfTechnicians,
-        numberOfTrucks,
-    } = day;
+	const { day, index } = props;
+	const {
+		deliveryRounds,
+		installationRounds,
+		numberOfTechnicians,
+		numberOfTrucks,
+	} = day;
 
-    const [deliveryCollapse, setDeliveryCollapse] =
-        React.useState<boolean>(false);
+	const [deliveryCollapse, setDeliveryCollapse] =
+		React.useState<boolean>(false);
 
-    const [installationCollapse, setInstallationCollapse] =
-        React.useState<boolean>(false);
+	const [installationCollapse, setInstallationCollapse] =
+		React.useState<boolean>(false);
 
-    const [globalCollapse, setGlobalCollapse] = React.useState<boolean>(false);
+	const [globalCollapse, setGlobalCollapse] = React.useState<boolean>(false);
 
-    const onClickDelivery = () => setDeliveryCollapse(!deliveryCollapse);
+	const onClickDelivery = () => setDeliveryCollapse(!deliveryCollapse);
 
-    const onClickInstallation = () =>
-        setInstallationCollapse(!installationCollapse);
+	const onClickInstallation = () =>
+		setInstallationCollapse(!installationCollapse);
 
-    const onClickGlobal = () => setGlobalCollapse(!globalCollapse);
+	const onClickGlobal = () => setGlobalCollapse(!globalCollapse);
 
-    const renderDeliveryRounds = () => {
-        if (!deliveryRounds) return null;
+	const renderDeliveryRounds = () => {
+		if (!deliveryRounds) return null;
 
-        return deliveryRounds.map((deliveryRound, indexRound) => (
-            <DeliveryRound
-                deliveryRound={deliveryRound}
-                index={indexRound}
-                key={`deliveryRound-${index}-${indexRound}`}
-            />
-        ));
-    };
+		return deliveryRounds.map((deliveryRound, indexRound) => (
+			<DeliveryRound
+				deliveryRound={deliveryRound}
+				index={indexRound}
+				key={`deliveryRound-${index}-${indexRound}`}
+			/>
+		));
+	};
 
-    const renderInstallationRounds = () => {
-        if (!installationRounds) return null;
+	const renderInstallationRounds = () => {
+		if (!installationRounds) return null;
 
-        return installationRounds.map((installationRound, indexRound) => (
-            <InstallationRound
-                index={indexRound}
-                installationRound={installationRound}
-                key={`installationRound-${index}-${indexRound}`}
-            />
-        ));
-    };
+		return installationRounds.map((installationRound, indexRound) => (
+			<InstallationRound
+				index={indexRound}
+				installationRound={installationRound}
+				key={`installationRound-${index}-${indexRound}`}
+			/>
+		));
+	};
 
-    const renderGlobalView = () => {
-        if (!installationRounds && !deliveryRounds) return null;
+	const renderGlobalView = () => {
+		if (!installationRounds && !deliveryRounds) return null;
 
-        return (
-            <>
-                <p className="italic pointer" onClick={onClickGlobal}>
-                    Global view...
-                </p>
-                <Collapse isOpened={globalCollapse}>
-                    {deliveryRounds ? (
-                        <>
-                            <p>Delivery Round</p>
-                            <GlobalCanvas rounds={deliveryRounds} />
-                        </>
-                    ) : null}
-                    {installationRounds ? (
-                        <>
-                            <p>Installation Round</p>
-                            <GlobalCanvas rounds={installationRounds} />
-                        </>
-                    ) : null}
-                </Collapse>
-            </>
-        );
-    };
+		return (
+			<>
+				<p className="italic pointer" onClick={onClickGlobal}>
+					Global view...
+				</p>
+				<Collapse isOpened={globalCollapse}>
+					<>
+						{renderGlobalDeliveryRounds()}
+						{renderGlobalInstallationRounds()}
+					</>
+				</Collapse>
+			</>
+		);
+	};
 
-    return (
-        <div className="day">
-            <div className="dayBanner">Day {index + 1}</div>
-            {renderGlobalView()}
-            <p>Number of trucks : {numberOfTrucks}</p>
-            <p className="italic pointer" onClick={onClickDelivery}>
-                Delivery Round details...
-            </p>
-            <Collapse isOpened={deliveryCollapse}>
-                {renderDeliveryRounds()}
-            </Collapse>
-            <p>Number of technicians : {numberOfTechnicians}</p>
-            <p className="italic pointer" onClick={onClickInstallation}>
-                Installation Round details...
-            </p>
-            <Collapse isOpened={installationCollapse}>
-                {renderInstallationRounds()}
-            </Collapse>
-        </div>
-    );
+	const renderGlobalDeliveryRounds = () => {
+		if (!deliveryRounds || !deliveryRounds) return null;
+
+		return (
+			<>
+				<p>Delivery Round</p>
+				<div className="globalView">
+					{deliveryRounds.map((deliveryRound, index) => {
+						const { origin, requests } = deliveryRound;
+						const color = colors[index % colors.length];
+
+						return (
+							<Canvas
+								classname="canvas"
+								color={color}
+								key={`delivery-${origin.x}-${origin.y}-${index}`}
+								origin={origin}
+								requests={requests}
+							/>
+						);
+					})}
+				</div>
+			</>
+		);
+	};
+
+	const renderGlobalInstallationRounds = () => {
+		if (!installationRounds || !installationRounds.length) return null;
+
+		return (
+			<>
+				<p>Installation Round</p>
+				<div className="globalView">
+					{installationRounds.map((deliveryRound, index) => {
+						const { origin, requests } = deliveryRound;
+						const color = colors[index % colors.length];
+
+						return (
+							<Canvas
+								classname="canvas"
+								color={color}
+								key={`installation-${origin.x}-${origin.y}-${index}`}
+								origin={origin}
+								requests={requests}
+							/>
+						);
+					})}
+				</div>
+			</>
+		);
+	};
+
+	return (
+		<div className="day">
+			<div className="dayBanner">Day {index + 1}</div>
+			{renderGlobalView()}
+			<p>Number of trucks : {numberOfTrucks}</p>
+			{deliveryRounds ? (
+				<>
+					<p className="italic pointer" onClick={onClickDelivery}>
+						Delivery Round details...
+					</p>
+					<Collapse isOpened={deliveryCollapse}>
+						{renderDeliveryRounds()}
+					</Collapse>
+				</>
+			) : null}
+			<p>Number of technicians : {numberOfTechnicians}</p>
+			{installationRounds ? (
+				<>
+					<p className="italic pointer" onClick={onClickInstallation}>
+						Installation Round details...
+					</p>
+					<Collapse isOpened={installationCollapse}>
+						{renderInstallationRounds()}
+					</Collapse>
+				</>
+			) : null}
+		</div>
+	);
 };
 
 export { Day };
